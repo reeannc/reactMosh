ff439cfcbae779ba5232c1e52661682c0ca93530
Reeann Chloe Albert <rcalbertofc@gmail.com>
2023-12-06 18:48:11 -0500

    added onSelectItem

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import ListGroup from "./components/ListGroup";
 
-function App(){
-  return <div><ListGroup/></div>
+function App() {
+  let items = ["New York", "Baguio", "Paris"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+    </div>
+  );
 }
 
 export default App;
+
+//when onSelectItem, first needs a function handleSelectItem
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,21 @@
 import { useState } from "react";
 
-function ListGroup() {
-  let items = ["New York", "Baguio", "Paris"];
+//props = inputs
+// {items: [], heading: string}
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
   //Hook, used to update states
   const [selectedIndex, setSelectedIndex] = useState(-1);
   //selectedIndex, setSelectedIndex = variable, updating function
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
@@ -21,6 +28,7 @@ function ListGroup() {
             key={item}
             onClick={() => {
               setSelectedIndex(index);
+              onSelectItem(item);
             }}
           >
             {item}
@@ -32,3 +40,11 @@ function ListGroup() {
 }
 
 export default ListGroup;
+
+//function to add details = do not implement that logic inside this component
+//= wont be reusable component
+
+//props are Immutable vs state wc is immutable
+//anytime props or state are re-rendered/changed = updates DOM
+
+//add other details when selected
